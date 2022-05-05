38ac5da67b47fc93c3be83c8f2897621aceb8bf2
Toni Galmés Oliver <tonigalmes98@gmail.com>
2022-05-05 12:24:53 +0200

    fix IBeefyVaultV6 path

--- contracts/BeefyBattlesEventV1.sol
+++ contracts/BeefyBattlesEventV1.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "./interfaces/IBeefyValutV6.sol";
+import "../interfaces/IBeefyValutV6.sol";
 
 /// @title Beefy Battles Event
 /// @author Galmoli
